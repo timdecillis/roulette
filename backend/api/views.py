4872f546a8b437c7abf6be6b98bd6f2c32751007
timdecillis <timdecillis@gmail.com>
2024-07-15 15:52:16 -0700

    created views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class Button1View(APIView):
+    def get(self, request):
+        data = {"message": "Hello from button 1"}
+        return Response(data, status=status.HTTP_200_OK)
+
+class Button2View(APIView):
+    def get(self, request):
+        data = {"message": "Hello from button 2"}
+        return Response(data, status=status.HTTP_200_OK)
