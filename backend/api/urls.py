c36b9ab70ecc18a9968e4083a2fc225052eb55d5
timdecillis <timdecillis@gmail.com>
2024-07-15 16:01:15 -0700

    added views

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/endpoint1/', views.Endpoint1View.as_view(), name='endpoint1'),
-    path('api/endpoint2/', views.Endpoint2View.as_view(), name='endpoint2'),
+    path('button1/', views.Button1View.as_view(), name='endpoint1'),
+    path('button2/', views.Button2View.as_view(), name='endpoint2'),
 ]
