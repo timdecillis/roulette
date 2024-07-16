6c80e0d8b6b4310cc55a02cbe6e746813e516e94
timdecillis <timdecillis@gmail.com>
2024-07-16 15:54:41 -0700

    updated first endpoint

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import random
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -6,7 +7,10 @@ from rest_framework import status
 
 class Button1View(APIView):
     def get(self, request):
-        data = {"message": "Hello from button 1"}
+        # x = round(5.76543)
+        number = random.randint(0, 100)
+        data = {"message": number}
+
         return Response(data, status=status.HTTP_200_OK)
 
 class Button2View(APIView):
