a2a78e3b87ce77800deafca80916b7934e45b8d6
timdecillis <timdecillis@gmail.com>
2024-07-18 11:18:08 -0700

    added error handling in requests.ts

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -3,14 +3,26 @@ import axios from "axios";
 const baseURL = "http://localhost:8000";
 
 export async function handleNumberRequest() {
-  const response = await axios.get(baseURL + "/button1/");
-  const { data } = response;
-  return data;
+  try {
+    const response = await axios.get(baseURL + "/button1/");
+    const { data } = response;
+    return data;
+  } catch (error) {
+    console.error(
+      `The following network error occurred while making a request for a random number: ${error}`
+    );
+  }
 }
 export async function handleColorRequest(color: string) {
-  const response = await axios.post(baseURL + "/button2/", {
-    currentColor: color,
-  });
-  const { data } = response;
-  return data;
+  try {
+    const response = await axios.post(baseURL + "/button2/", {
+      currentColor: color,
+    });
+    const { data } = response;
+    return data;
+  } catch (error) {
+    console.error(
+      `The following network error occurred while making a request for a color: ${error}`
+    );
+  }
 }
